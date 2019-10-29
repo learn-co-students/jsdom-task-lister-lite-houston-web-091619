992552409644ce0ff2f61dc32dd9888f05972434
github username <github email address>
2019-10-29 16:16:45 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const form = document.getElementById('create-task-form')
+
+
+      form.addEventListener("submit", function() {
+      event.preventDefault()
+
+          function creatList() {
+              const create_li = document.createElement('li')
+              const create_Btn = document.createElement('button')
+              create_Btn.innerText = "X"
+              const get_ul = document.getElementById('tasks')
+              create_li.innerText = event.target[0].value
+              create_li.append(create_Btn)
+              get_ul.append(create_li)
+                  create_Btn.addEventListener("click", function(){
+                      create_li.remove()
+                      create_Btn.remove()
+                  });
+          };
+        creatList() 
+    });
+
+
+    const deleteBtn = document.getElementById('clear-btn')
+        deleteBtn.addEventListener("click", function(){
+        alert("hello")
+    });
+
+     
 });
+
