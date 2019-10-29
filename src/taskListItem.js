7626d4fc599fed2f3eb519f318201e33ef8c79bc
Damini varu <daminivaru@Daminis-MacBook-Pro.local>
2019-10-29 17:25:48 -0500

    Done.

---
+++ src/taskListItem.js
@@ -0,0 +1,16 @@
+class TaskListItem {
+
+    constructor(description){
+      this.element = document.createElement('li')
+      this.element.innerText = description
+  
+      const removeButton = document.createElement('button')
+      removeButton.innerText = 'Remove'
+      this.element.append(removeButton)
+  
+      removeButton.addEventListener('click', () =>{
+        this.element.remove()
+      })
+    }
+  
+  }
