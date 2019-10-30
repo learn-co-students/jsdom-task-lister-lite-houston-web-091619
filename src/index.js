c18d9b02794259a901c505ef3c6ecda9f8319dfc
Fabi <fafi@Fabis-MBP.attlocal.net>
2019-10-29 21:11:03 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function (){
   // your code here
-});
+    const form = document.querySelector('#create-task-form')
+    form.addEventListener("submit", function() {
+      event.preventDefault()
+      // debugger
+      console.log(event.target[0].value)
+      // const div = document.querySelector('#description-container') 
+      const div = document.querySelector('#list')
+
+      const p = document.createElement('p')
+      p.innerText = event.target[0].value
+      div.append(p)
+      
+        event.target.reset()
+        
+      })
+})
